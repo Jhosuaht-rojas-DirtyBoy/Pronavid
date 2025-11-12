ad09df3a3ea5e602eb7c96528af2bc0902006fd1
NicolasM220x <Nue6689@gmail.com>
2025-11-12 13:13:48 -0500

    Actualización de App.jsx
    Se agregó el apartado de registro (Funcional con JSON)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
+import { useState } from "react";
 import Login from "./pages/login.jsx";
+import Registro from "./pages/registro.jsx";
 
 function App() {
-  return <Login />;
+  const [vista, setVista] = useState("login"); // "login" o "registro"
+
+  return (
+    <div>
+      {vista === "login" && (
+        <>
+          <Login />
+          <p style={{ textAlign: "center", marginTop: "10px" }}>
+            ¿No tienes cuenta?{" "}
+            <button onClick={() => setVista("registro")}>Registrarse</button>
+          </p>
+        </>
+      )}
+
+      {vista === "registro" && (
+        <>
+          <Registro />
+          <p style={{ textAlign: "center", marginTop: "10px" }}>
+            ¿Ya tienes cuenta?{" "}
+            <button onClick={() => setVista("login")}>Iniciar Sesión</button>
+          </p>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default App;
